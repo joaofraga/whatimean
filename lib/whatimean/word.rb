c991aec32f1fa27223718a22be89ed3ac9778a58
Joao Fraga <jgfraga@gmail.com>
2015-10-28 11:22:05 -0200

    WhatIMean::Word documentation

--- lib/whatimean/word.rb
+++ lib/whatimean/word.rb
@@ -1,7 +1,14 @@
 module WhatIMean
+
   class Word
     attr_accessor :name, :hits, :rate
 
+    # Instatiate a new Word with following attributes:
+    # @param [Hash] attributes Data that will be set in Word.
+    #
+    # @option attributes [String] :name Word name
+    # @option attributes [Fixnum] :hits Estimated hit count (occurrences)
+    # @option attributes [Float] :rate Rate comparison based on total value
     def initialize(attributes)
       attributes.each do |key, value|
         send("#{key}=", value)
@@ -10,6 +17,9 @@ module WhatIMean
       self
     end
 
+    # Calculate
+    # @param [Float] total The total ammount to be compared
+    # @return [Word] self object
     def calculate_rate(total)
       self.rate = hits / total.to_f
       self
