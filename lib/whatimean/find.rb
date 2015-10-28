ca86450fb9f9ea1d1f2d5fca76dafebf9572646e
Joao Fraga <jgfraga@gmail.com>
2015-10-28 11:21:24 -0200

    WhatIMean::Find documentation

--- lib/whatimean/find.rb
+++ lib/whatimean/find.rb
@@ -1,12 +1,21 @@
 module WhatIMean
+  # = Find
+  # The Core of WhatIMean, this class interacts with Google and Word and
+  # fetch the search returning each word with hits, name and rate comparison
   class Find
     attr_reader :words
 
+    # Compare strings and return a list ordered by occurence
+    # @param [Google] client The WhatIMean::Google client
+    # @param [Array<String>] words The words to be compared
+    # @return [Find] the words with hits counter and comparison rates
     def initialize(client, *words)
       @client = client
       @words = fetch_words(words).sort { |x,y| y.hits <=> x.hits }
     end
 
+    # Returns the word in comparison with most results
+    # @return [Word] The winner with most occurences
     def winner
       @words.first
     end
